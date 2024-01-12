a8b75cdd67b17ff03abaa660b3428a9bdc699009
stuartfogel <stuartfogel@192.168.5.5>
2024-01-12 13:42:52 -0500

    V1.1: first revision after EEGLAB plugin upload.

--- eegplugin_countingSheepPSG.m
+++ eegplugin_countingSheepPSG.m
@@ -91,7 +91,7 @@ function vers = eegplugin_countingSheepPSG(fig, trystrs, catchstrs)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % version
-vers = '1.0';
+vers = '1.1';
 
 % handle input arguments from EEGLAB
 if nargin < 3
