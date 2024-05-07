d8229bcf2ff116d3fb9e60d5bf2ebff5f76fd259
stuartfogel <stuartfogel@Stuarts-M2-MacBook-Air.local>
2024-05-07 07:01:34 -0400

    V1.4.2: Small but important additional fixes to multi-epoch mode display and navigation.

--- eegplugin_countingSheepPSG.m
+++ eegplugin_countingSheepPSG.m
@@ -89,7 +89,7 @@ function vers = eegplugin_countingSheepPSG(fig, trystrs, catchstrs)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % version
-vers = '1.4.1';
+vers = '1.4.2';
 
 % handle input arguments from EEGLAB
 if nargin < 3
