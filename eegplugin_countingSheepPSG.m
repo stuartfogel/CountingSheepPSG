fd87fdf5d486d8ba34190d16a318c3cd0d24c739
stuartfogel <stuartfogel@Stuarts-M2-MacBook-Air.local>
2025-01-09 14:03:43 -0500

    V1.5: compatability fixes for matlab R2024b

--- eegplugin_countingSheepPSG.m
+++ eegplugin_countingSheepPSG.m
@@ -95,7 +95,7 @@ function vers = eegplugin_countingSheepPSG(fig, trystrs, catchstrs)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % version
-vers = '1.4.7';
+vers = '1.5';
 
 % handle input arguments from EEGLAB
 if nargin < 3
