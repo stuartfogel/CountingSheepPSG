78662adf5a1a1e0ba9852e69e9580afc404ee462
stuartfogel <stuartfogel@Stuarts-M2-MacBook-Air.local>
2025-01-20 13:24:15 -0500

    V1.5.1: fixed event display issue for single channel events that span multiple epochs

--- eegplugin_countingSheepPSG.m
+++ eegplugin_countingSheepPSG.m
@@ -95,7 +95,7 @@ function vers = eegplugin_countingSheepPSG(fig, trystrs, catchstrs)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % version
-vers = '1.5';
+vers = '1.5.1'; % fixed event display issue for single channel events that span multiple epochs
 
 % handle input arguments from EEGLAB
 if nargin < 3
