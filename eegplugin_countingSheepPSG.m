8b206880da234dc4d10bef4852d842a0bffdd2a4
stuartfogel <stuartfogel@Stuarts-M2-MacBook-Air.local>
2025-02-03 11:25:14 -0500

    V1.5.5: included user manual. updated event properties for better R2024b compatability.

--- eegplugin_countingSheepPSG.m
+++ eegplugin_countingSheepPSG.m
@@ -95,7 +95,7 @@ function vers = eegplugin_countingSheepPSG(fig, trystrs, catchstrs)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % version
-vers = '1.5.4'; % updated sleep report generation to account for issues around lights on/off tags
+vers = '1.5.5'; % included user manual. updated event properties for better R2024b compatability.
 
 % handle input arguments from EEGLAB
 if nargin < 3
