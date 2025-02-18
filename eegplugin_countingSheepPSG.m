1280beb3131d31d2fc705ac762b9dce0b3f3fa7f
stuartfogel <stuartfogel@Stuarts-M2-MacBook-Air.local>
2025-02-18 15:25:07 -0500

    V1.6: A number of updates to remove legacy date format. Improved message dialogs. Misc. improvements.

--- eegplugin_countingSheepPSG.m
+++ eegplugin_countingSheepPSG.m
@@ -95,7 +95,7 @@ function vers = eegplugin_countingSheepPSG(fig, trystrs, catchstrs)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % version
-vers = '1.5.6'; % improved precision of sleep report stats.
+vers = '1.6'; % A number of updates to remove legacy date format. Improved message dialogs. Misc. improvements.
 
 % handle input arguments from EEGLAB
 if nargin < 3
