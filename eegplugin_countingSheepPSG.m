4df2a4260e8c4422be581738ea5104920faf1e5f
stuartfogel <stuartfogel@Stuarts-M2-MacBook-Air.local>
2025-05-24 07:56:22 -0400

    V1.10: email provider blocked sendmail. temporary fix.

--- eegplugin_countingSheepPSG.m
+++ eegplugin_countingSheepPSG.m
@@ -95,7 +95,7 @@ function vers = eegplugin_countingSheepPSG(fig, trystrs, catchstrs)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % version
-vers = '1.9.1'; % improved error handling for update and registration
+vers = '1.10'; % email provider blocked sendmail. temporary fix. 
 
 % handle input arguments from EEGLAB
 if nargin < 3
