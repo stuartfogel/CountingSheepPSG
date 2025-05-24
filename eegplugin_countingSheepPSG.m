c903545f1752f51a545b78514159236fab8c955d
stuartfogel <stuartfogel@Stuarts-M2-MacBook-Air.local>
2025-05-24 08:13:26 -0400

    V1.11: email provider service restored. registration temp fix removed.

--- eegplugin_countingSheepPSG.m
+++ eegplugin_countingSheepPSG.m
@@ -95,7 +95,7 @@ function vers = eegplugin_countingSheepPSG(fig, trystrs, catchstrs)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % version
-vers = '1.10'; % email provider blocked sendmail. temporary fix. 
+vers = '1.11'; % email provider service restored. registration temp fix removed.  
 
 % handle input arguments from EEGLAB
 if nargin < 3
