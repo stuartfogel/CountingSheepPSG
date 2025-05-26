5404008c1e5efe2fad611f2e4fc55828815a97dc
stuartfogel <stuartfogel@Stuarts-M2-MacBook-Air.local>
2025-05-26 13:11:24 -0400

    V1.12: Small bug fix in sleep stage loading.

--- eegplugin_countingSheepPSG.m
+++ eegplugin_countingSheepPSG.m
@@ -95,7 +95,7 @@ function vers = eegplugin_countingSheepPSG(fig, trystrs, catchstrs)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % version
-vers = '1.11'; % email provider service restored. registration temp fix removed.  
+vers = '1.12'; % Small bug fix in sleep stage loading.
 
 % handle input arguments from EEGLAB
 if nargin < 3
