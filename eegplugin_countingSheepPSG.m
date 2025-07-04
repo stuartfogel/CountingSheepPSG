49a3093f278b8f875df8377be63da4637757910f
stuartfogel <stuartfogel@Stuarts-M2-MacBook-Air.local>
2025-07-04 12:27:59 -0400

    V1.18: Improved on system-speicfic registration issues. Updated for MATLAB R2025a. Other minor performace and code enhancements.

--- eegplugin_countingSheepPSG.m
+++ eegplugin_countingSheepPSG.m
@@ -95,7 +95,7 @@ function vers = eegplugin_countingSheepPSG(fig, trystrs, catchstrs)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % version
-vers = '1.17'; % fixed issue when filtering multiple channel types
+vers = '1.18'; % Improved on system-speicfic registration issues. Updated for MATLAB R2025a. Other minor performace and code enhancements.
 
 % handle input arguments from EEGLAB
 if nargin < 3
