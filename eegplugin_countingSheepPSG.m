2a1f5ef3b4a5c7b8d81fa12c5b4d961d8e981e2a
stuartfogel <stuartfogel@Stuarts-M2-MacBook-Air.local>
2025-07-08 17:06:26 -0400

    V1.19: More MATLAB R2025a compatability updates. Other error-catching, minor performace and code enhancements.

--- eegplugin_countingSheepPSG.m
+++ eegplugin_countingSheepPSG.m
@@ -95,7 +95,7 @@ function vers = eegplugin_countingSheepPSG(fig, trystrs, catchstrs)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % version
-vers = '1.18'; % Improved on system-speicfic registration issues. Updated for MATLAB R2025a. Other minor performace and code enhancements.
+vers = '1.19'; % More MATLAB R2025a compatability updates. Other error-catching, minor performace and code enhancements.
 
 % handle input arguments from EEGLAB
 if nargin < 3
