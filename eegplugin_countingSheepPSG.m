9b89f064bf96531a02b47d7a80d5d94d0fd2d026
stuartfogel <stuartfogel@Stuarts-M2-MacBook-Air.local>
2025-07-22 13:11:23 -0400

    V1.22: New and improved montage editor!

--- eegplugin_countingSheepPSG.m
+++ eegplugin_countingSheepPSG.m
@@ -95,7 +95,7 @@ function vers = eegplugin_countingSheepPSG(fig, trystrs, catchstrs)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % version
-vers = '1.21'; % Fixed rounding imprecision in sleep reports. Minor improvements and error-catching.
+vers = '1.22'; % New and improved montage editor!
 
 % handle input arguments from EEGLAB
 if nargin < 3
