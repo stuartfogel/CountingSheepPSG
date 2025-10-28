2bdce62ed8a26646a1acf39a480a54c5017fd0c0
stuartfogel <stuartfogel@Stuarts-M2-MacBook-Air.local>
2025-10-28 11:48:13 -0400

    V1.29: force save of stages to backup mat on close dataset. Display ICA menu item after running ICA.

--- eegplugin_countingSheepPSG.m
+++ eegplugin_countingSheepPSG.m
@@ -95,7 +95,7 @@ function vers = eegplugin_countingSheepPSG(fig, trystrs, catchstrs)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % version
-vers = '1.28'; % now forces save of stages to backup mat on clear dataset.
+vers = '1.29'; % force save of stages to backup mat on close dataset. Display ICA menu item after running ICA.
 
 % handle input arguments from EEGLAB
 if nargin < 3
