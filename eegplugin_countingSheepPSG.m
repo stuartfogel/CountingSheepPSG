213fa5edb00e178f800363ba7d894065c6c2c7c4
stuartfogel <stuartfogel@Stuarts-M2-MacBook-Air.local>
2026-01-28 11:51:44 -0500

    V1.36: minor update to clear data outside lights tags

--- eegplugin_countingSheepPSG.m
+++ eegplugin_countingSheepPSG.m
@@ -95,7 +95,7 @@ function vers = eegplugin_countingSheepPSG(fig, trystrs, catchstrs)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % version
-vers = '1.35'; % minor update to edit setname function
+vers = '1.36'; % minor update to clear data outside lights tags
 
 % handle input arguments from EEGLAB
 if nargin < 3
