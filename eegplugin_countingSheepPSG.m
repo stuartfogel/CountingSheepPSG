ea7ad47f5b24ae3f5395b7df114420366405f120
stuartfogel <stuartfogel@Stuarts-M2-MacBook-Air.local>
2026-04-01 11:49:03 -0400

    V1.41: minor bugfix when manually entering registration key

--- eegplugin_countingSheepPSG.m
+++ eegplugin_countingSheepPSG.m
@@ -102,7 +102,7 @@ function vers = eegplugin_countingSheepPSG(fig, trystrs, catchstrs)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % version
-vers = '1.40'; % updated license
+vers = '1.41'; % minor bugfix when manually entering registration key
 
 % handle input arguments from EEGLAB
 if nargin < 3
