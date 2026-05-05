2f1eca874792f324855839975bf8630ba11175c3
stuartfogel <stuartfogel@Stuarts-M2-MacBook-Air.local>
2026-05-05 13:31:29 -0400

    V1.43: handle event display issue when EEGLAB 'boundary' events have NaN as duration

--- eegplugin_countingSheepPSG.m
+++ eegplugin_countingSheepPSG.m
@@ -102,7 +102,7 @@ function vers = eegplugin_countingSheepPSG(fig, trystrs, catchstrs)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % version
-vers = '1.42'; % yet another minor bugfix when manually entering registration key
+vers = '1.43'; % handle event display issue when EEGLAB 'boundary' events have NaN as duration
 
 % handle input arguments from EEGLAB
 if nargin < 3
