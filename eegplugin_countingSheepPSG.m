922fa92fab2f685795ae44244a59d21c546d4e58
stuartfogel <stuartfogel@Stuarts-M2-MacBook-Air.local>
2026-05-27 10:38:07 -0400

    V1.45: minor fix to brute-force edf import and auto light on/off tag insert

--- eegplugin_countingSheepPSG.m
+++ eegplugin_countingSheepPSG.m
@@ -102,7 +102,7 @@ function vers = eegplugin_countingSheepPSG(fig, trystrs, catchstrs)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % version
-vers = '1.44'; % registration process update
+vers = '1.45'; % minor fix to brute-force edf import and auto light on/off tag insert
 
 % handle input arguments from EEGLAB
 if nargin < 3
